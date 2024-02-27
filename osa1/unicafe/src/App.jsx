0d7f7357b732a02d42d115f1d4ce8a1060875478
Antti Aitta <n1aian00@students.oamk.fi>
2024-02-27 16:40:53 +0200

    1.7 valmis

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,14 +1,49 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const increaseByOne = () => {
+    console.log('+1')
+    setGood(good +1)
+  }
+
+  const stayStill = () => {
+    console.log('+-0')
+    setNeutral(neutral +1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('-1')
+    setBad(bad +1)
+  }
+
+  const total = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
+  const  positivePercent = total == 0 ? 0 : (good / total) * 100
+
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <Button handleClick={increaseByOne} text='good' />
+      <Button handleClick={stayStill} text='neutral' />
+      <Button handleClick={decreaseByOne} text='bad' />
+      <h1>statics</h1>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average}</div>
+      <div>positive {positivePercent} %</div>
     </div>
   )
 }
