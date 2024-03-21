3e57a665f439a0f4dbcd293f3f0e6d59aedf6b15
Antti Aitta <n1aian00@students.oamk.fi>
2024-03-21 16:24:07 +0200

    3.2 valmis

--- osa3/index.js
+++ osa3/index.js
@@ -1,33 +1,44 @@
-const http = require('http')
+const http = require('http');
 
 let notes = [
     {
-      id: 1,
-      name: "Arto Hellas",
-      number: "040-123456"
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
     },
     {
-      id: 2,
-      name: "Ada Lovelace",
-      number: "39-44-5323523"
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
     },
     {
-      id: 3,
-      name: "Dan Abramov",
-      number: "12-43-234345"
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
     },
     {
-      id: 4,
-      name: "Mary Poppendick",
-      number: "39-23-6423122"
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
     }
-  ]
-  
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
+]
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const app = http.createServer((request, response) => {
+    if (request.url === '/info' && request.method === 'GET') {
+        const contactCount = notes.length;
+
+        const currentTime = new Date().toLocaleString()
+
+        const infoResponse = `Phonebook has info for ${contactCount} people\n${currentTime}`;
+
+        response.writeHead(200, { 'Content-Type': 'text/plain' });
+        response.end(infoResponse);
+    } else {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(notes));
+    }
+});
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
